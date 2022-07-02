c1eb711f37d9c0da4110272832aac45fa13b17f6
agmlage <agmlage@gmail.com>
2022-07-02 17:46:28 -0300

    02/07/22

---
+++ README.md
@@ -0,0 +1,7 @@
+https://www2.correios.com.br/sistemas/buscacep/buscaFaixaCep.cfm
+pip install beautifulsoup4
+pip install requests
+
+Localidade
+Faixa de cep
+Gerar id
---
+++ buscauf.py
@@ -0,0 +1,19 @@
+import requests
+from requests.structures import CaseInsensitiveDict
+from bs4 import BeautifulSoup
+
+url = "https://www2.correios.com.br/sistemas/buscacep/resultadoBuscaFaixaCEP.cfm"
+
+headers = CaseInsensitiveDict()
+headers["Content-Type"] = "application/x-www-form-urlencoded"
+
+data = ""
+
+
+resp = requests.post(url, headers=headers, data=data)
+
+soup = BeautifulSoup(resp.text, 'html.parser')
+
+ufs = soup.find_all("select", { "name" : "UF" })
+for uf in ufs:
+    print(uf.text)
