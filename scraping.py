29c35e7845c970beec78c08d848be75f38ae4871
agmlage <agmlage@gmail.com>
2022-07-06 11:57:20 -0300

    Final com ID errado

--- scraping.py
+++ scraping.py
@@ -4,10 +4,10 @@ from requests.structures import CaseInsensitiveDict
 from bs4 import BeautifulSoup
 import pandas as pd
 import numpy as np
-#from pyoccur import pyoccur
+import json
 
 url = "https://www2.correios.com.br/sistemas/buscacep/resultadoBuscaFaixaCEP.cfm"
-id=1
+idlocalidade=1
 board_members = []
 #pagini=1
 headers = CaseInsensitiveDict()
@@ -24,70 +24,49 @@ def consulta_uf(headers):
         string = uf.text
     lista = string.split()
     #print (lista)
-    return lista
-    
+    return lista   
 
-def consulta_cep(uf,id,url,headers):
-    #Percorrer o vetor buscando informações das UFs
-    for cont in range (20):
-        lista=uf
+def consulta_cep(uf,idlocalidade):
+    print("# UF: " + uf)
+    pagini = 1
+    while pagini<900:
+        new = 0
         pagfin=pagini+99
-        #data = "UF="+lista[cont]+"&pagini="+str(pagini)+"&pagfim="+str(pagfin)
-        data = "UF=SP"+"&pagini="+str(pagini)+"&pagfim="+str(pagfin)
-
+        data = "UF="+str(uf)+"&pagini="+str(pagini)+"&pagfim="+str(pagfin)
         resp = requests.post(url, headers=headers, data=data)
         soup = BeautifulSoup(resp.text, 'html.parser')
         tables = soup.find_all("table", { "class" : "tmptabela" })
         for table in tables:
-
             tabledata= table.find_all ("tr")
-        for tr in tabledata:
-            tddata = tr.find_all("td")
-            if len(tddata)==4:
-                board_members.append((lista[cont],tddata[0].text.strip(), tddata[1].text.strip(),id))
-                id +=1
+            for tr in tabledata:
+                tddata = tr.find_all("td")
+                if len(tddata)==4:
+                    localidade = {
+                        "Localidade": tddata[0].text.strip(),
+                        "Faixa de CEP": tddata[1].text.strip(),
+                        "ID": idlocalidade
+                    }
+                    skip = False
+                    for board_member in board_members:
+                        if board_member["Localidade"] == localidade["Localidade"]:
+                            skip = True
+                            break
+                    if not skip:
+                        board_members.append(localidade)
+                        idlocalidade +=1
+                        new += 1
+        pagini += 100
+        print("\tNew = " + str(new) + "\n\tpagini: " + str(pagini))
+        if new == 0:
+            break 
 
-        board_array = np.asarray(board_members)
-        len(board_array)
-        df = pd.DataFrame(board_array)
-        df.columns = ['UF','Localidade', 'Faixa de CEP','id']
-        df.to_csv('\Projeto\projeto-coleta-py\members.csv')
+def save_json():
+    stringfile = ""
+    for line in board_members:
+        stringfile = stringfile + json.dumps(line) + "\n"
+    with open("ufs.jsonl", "w") as fp:
+        fp.write(stringfile)
 
-def teste(uf,id,url,headers):
-    pagfin=pagini+99
-    data = "UF="+str(uf)+"&pagini="+str(pagini)+"&pagfim="+str(pagfin)
-    resp = requests.post(url, headers=headers, data=data)
-    soup = BeautifulSoup(resp.text, 'html.parser')
-    tables = soup.find_all("table", { "class" : "tmptabela" })
-    for table in tables:
-        tabledata= table.find_all ("tr")
-    for tr in tabledata:
-        tddata = tr.find_all("td")
-        if len(tddata)==4:
-            board_members.append((str(uf),tddata[0].text.strip(), tddata[1].text.strip(),id))
-            id +=1
-    board_array = np.asarray(board_members)
-    len(board_array)
-    #print (board_array)
-    df = pd.DataFrame(board_array)
-    #print (df)
-    df.columns = ['UF','Localidade', 'Faixa de CEP','id']
-    #df.drop_duplicates(subset='Localidade')
-    #print(df)
-    #return df
-    df.to_csv('\Projeto\projeto-coleta-py\members.csv')
-    return id
-def repini(pagini,uf,id,url,headers):
-    while pagini<900:
-        var=teste(uf,id,url,headers)
-        pagini+=100
-    return var
-
-lista=consulta_uf(headers)
-#print(lista)
-#df=teste(consulta_uf(headers),id,url,headers)
-for cont in range (len(lista)):
-    uf=lista[cont]
-    print (uf)
-    pagini=1
-    id=repini(pagini,uf,id,url,headers)
+for uf in consulta_uf(headers):
+    consulta_cep(uf,idlocalidade)
+save_json()
